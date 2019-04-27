9fdf5b3b6615dcebe380ac070f4bdfc032723a8a
Carlos Torres <carlosandrestorres28@hotmail.com>
2019-04-27 17:48:39 -0500

    added columns for visit count and title

--- db/migrate/20190427194626_create_shortener_urls.rb
+++ db/migrate/20190427194626_create_shortener_urls.rb
@@ -3,6 +3,8 @@ class CreateShortenerUrls < ActiveRecord::Migration[5.2]
     create_table :shortener_urls do |t|
       t.string :original_url
       t.string :shorten_url
+      t.integer :visits, default: 0
+      t.string :title, default: 'Pending...'
 
       t.timestamps
     end
