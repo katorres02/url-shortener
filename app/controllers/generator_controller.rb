e99300dcf022a91505b7c4776e5f223019e0ef01
Carlos Torres <carlosandrestorres28@hotmail.com>
2019-04-28 17:29:01 -0500

    verify if token is valid before redirecting

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -3,10 +3,16 @@ class GeneratorController < ApplicationController
 
   def show
     @response = Shortener::Url.read(params[:id])
-
-    respond_to do |format|
-      format.html { redirect_to @response }
-      format.json { render json: { response: @response }, status: :ok }
+    if @response[:status] == :ok
+      respond_to do |format|
+        format.html { redirect_to @response[:url] }
+        format.json { render json: { url: @response[:url] }, status: :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { render html: 'Invalid short url' }
+        format.json { render json: { message: @response.message }, status: :ok }
+      end
     end
   end
 
